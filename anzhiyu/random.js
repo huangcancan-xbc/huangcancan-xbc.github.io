4a7af77d7bcb2c3a8838ba43d3a80fed9cb9969c
小米里的大麦 <hc1960074081@qq.com>
2025-04-06 15:09:05 +0800

    Site updated: 2025-04-06 15:09:04

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/01/02 类与对象（上）/","2025/04/02/03 类与对象（中）/","2025/01/01/obsidian使用/","2025/04/03/04 类与对象（下）/"];function toRandomPost(){
+var posts=["2025/04/01/02 类与对象（上）/","2025/04/03/04 类与对象（下）/","2025/04/02/03 类与对象（中）/","2025/01/01/obsidian使用/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
