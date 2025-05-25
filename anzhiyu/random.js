8e0c7b3a384432ed1eb1789d48498011b9057d12
小米里的大麦 <hc1960074081@qq.com>
2025-05-25 13:34:10 +0800

    Site updated: 2025-05-25 13:34:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/49772.html","posts/42463.html","posts/55415.html","posts/15646.html","posts/35315.html","posts/24103.html","posts/9242.html","posts/30432.html","posts/62462.html","posts/57061.html","posts/10381.html","posts/47059.html","posts/4661.html"];function toRandomPost(){
+var posts=["posts/49772.html","posts/55415.html","posts/42463.html","posts/15646.html","posts/35315.html","posts/24103.html","posts/9242.html","posts/30432.html","posts/62462.html","posts/47059.html","posts/57061.html","posts/10381.html","posts/4661.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
